de916fcf5751ad9c81f8e85b3d72152357596a6a
DavidDong416 <dk@Kes-MacBook-Pro.local>
2017-02-23 11:31:18 -0500

    Yeoman Setup
    This is the setup of the application using the Yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pubNubChatApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pubNubChatApp
+ */
+angular.module('pubNubChatApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pubNubChatApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pubNubChatApp
+ */
+angular.module('pubNubChatApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
